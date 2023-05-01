b794302ed7bd4009ecb6c968f711f9e29d56abfa
yjw0514 <jwjwhappy92@gmail.com>
2023-05-01 14:38:11 +0900

    feat. primary 컬러 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FA6FA9',
+      },
+    },
   },
   plugins: [],
 };
