6ced4d8da941c3a4919c8fe5339f9f6c07673d53
yjw0514 <jwjwhappy92@gmail.com>
2023-05-21 22:10:06 +0900

    feat. react query 로 비동기 작업, 로그인 회원가입 api 통신

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,18 @@ import PageLayout from '@/components/Layout';
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
   return (
-    <PageLayout>
-      <Head>
-        <link rel="icon" href="/image/logo.ico" sizes="any" />
-      </Head>
-      <Component {...pageProps} />
-    </PageLayout>
+    <QueryClientProvider client={queryClient}>
+      <PageLayout>
+        <Head>
+          <link rel="icon" href="/image/logo.ico" sizes="any" />
+        </Head>
+        <Component {...pageProps} />
+      </PageLayout>
+    </QueryClientProvider>
   );
 }
