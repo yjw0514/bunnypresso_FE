bd6d7da21f45074b47e21209b3513620582c6d03
yjw0514 <jwjwhappy92@gmail.com>
2023-05-30 21:33:08 +0900

    feat. 새로고침 하는 경우에도 로그인여부 상태값 유지되도록 redux-persisit 적용

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import authReducer from '@/store/slice/authSlice';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
+const persistConfig = {
+  key: 'root', // localStorage key
+  storage, // localStorage
+  whitelist: ['auth'], // target (reducer name)
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
