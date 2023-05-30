648db83b23e1a38d6065eb6148f21ae7c9cb3062
yjw0514 <jwjwhappy92@gmail.com>
2023-05-30 22:54:45 +0900

    feat. 로딩 컴포넌트 생성

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <img
+      src="https://banapresso.com/mo/static/media/img_map_loading.c5d15d46f6c2396432fa.gif"
+      alt="loading"
+    />
+  );
+};
+
+export default Loading;
