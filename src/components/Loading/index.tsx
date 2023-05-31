6f8a076fb09c210c799af9c3f56739f04d7ce2c0
yjw0514 <jwjwhappy92@gmail.com>
2023-05-31 23:52:19 +0900

    feat. 로딩 컴포넌트 스타일 수정

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,9 +1,12 @@
 const Loading = () => {
   return (
-    <img
-      src="https://banapresso.com/mo/static/media/img_map_loading.c5d15d46f6c2396432fa.gif"
-      alt="loading"
-    />
+    <div className="fixed top-0 bottom-0 left-0 right-0 flex-center">
+      <img
+        className="w-1/2"
+        src="https://banapresso.com/mo/static/media/img_map_loading.c5d15d46f6c2396432fa.gif"
+        alt="loading"
+      />
+    </div>
   );
 };
 
