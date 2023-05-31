66307676aa5d9f54b5bb066174f4ab103c4b79f3
yjw0514 <jwjwhappy92@gmail.com>
2023-06-01 01:00:33 +0900

    feat. 로딩 컴포넌트 수정

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,8 +1,8 @@
 const Loading = () => {
   return (
-    <div className="fixed top-0 bottom-0 left-0 right-0 flex-center">
+    <div className="pt-[40%] ">
       <img
-        className="w-1/2"
+        className="w-1/2 mx-auto "
         src="https://banapresso.com/mo/static/media/img_map_loading.c5d15d46f6c2396432fa.gif"
         alt="loading"
       />
