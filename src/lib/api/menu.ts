0d08d2b819146920165b037766f6c65b939bde17
yjw0514 <jwjwhappy92@gmail.com>
2023-06-22 23:19:19 +0900

    feat. 메뉴 api 작업

--- src/lib/api/menu.ts
+++ src/lib/api/menu.ts
@@ -1,6 +1,11 @@
 import { orderType } from '@/dto/menuDto';
 import { instance } from '@/lib/api/index';
 
+// 메뉴 목록
+export const getMenu = (name?: string) => {
+  return instance.get(`/menu?name=${name}`);
+};
+
 // 메뉴 상세 정보 가져오기
 export const getMenuDetail = (id: string) => {
   return instance.get(`/menu/${id}`);
@@ -8,10 +13,10 @@ export const getMenuDetail = (id: string) => {
 
 // 주문 등록
 export const takeOrder = ({ userId, menu, isHot, count }: orderType) => {
-  return instance.post('/menu', { userId, menu, isHot, count });
+  return instance.post('/order', { userId, menu, isHot, count });
 };
 
 // 주문목록 가져오기
 export const getOrderList = () => {
-  return instance.get('/menu/list');
+  return instance.get('/order/list');
 };
