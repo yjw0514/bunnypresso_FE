639d39e055f375eb764ce044935e47a2d7dee5ff
yjw0514 <jwjwhappy92@gmail.com>
2023-07-13 14:45:18 +0900

    feat. og tag 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,10 +14,25 @@ export default function App({ Component, pageProps }: AppProps) {
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <QueryClientProvider client={queryClient}>
+          <Head>
+            <link rel="icon" href="/image/logo.ico" sizes="any" />
+            <title>버니프레소</title>
+            <meta
+              name="description"
+              content="맛있는 음료와 디저트를 즐겨보세요."
+              key="desc"
+            />
+            <meta property="og:title" content="버니프레소" />
+            <meta
+              property="og:description"
+              content="버니프레소에서 맛있는 음료와 디저트를 즐겨보세요."
+            />
+            <meta
+              property="og:image"
+              content="https://plus.unsplash.com/premium_photo-1671013032517-481b086eced1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+            />
+          </Head>
           <PageLayout>
-            <Head>
-              <link rel="icon" href="/image/logo.ico" sizes="any" />
-            </Head>
             <Component {...pageProps} />
           </PageLayout>
           {/* <ReactQueryDevtools initialIsOpen={false} position="top-left" /> */}
