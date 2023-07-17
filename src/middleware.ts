adaf1cc84e5c8e29dcc62646d0004a110ec92ce7
yjw0514 <jwjwhappy92@gmail.com>
2023-07-17 21:37:38 +0900

    Revert "test. vercel 배포 시 미들웨어에서 토큰 체크되지 않는지 테스트"
    This reverts commit f1d568f658bc75fc42f442fded63150dfd705e95.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  // if (request.nextUrl.pathname.startsWith('/story')) {
-  //   console.log('로그인 accessToken!', request.cookies.get('accessToken'));
-  //   console.log('로그인 refreshToken!', request.cookies.get('refreshToken'));
-  //   const accessToken = request.cookies.get('accessToken');
-  //   const refreshToken = request.cookies.get('refreshToken');
-  //   if (!accessToken || !refreshToken) {
-  //     console.log('미들웨어 체크 : 로그인 필요함');
-  //     return NextResponse.redirect(new URL('/?type=login', request.url));
-  //   }
-  // }
+  if (request.nextUrl.pathname.startsWith('/story')) {
+    console.log('로그인 accessToken!', request.cookies.get('accessToken'));
+    console.log('로그인 refreshToken!', request.cookies.get('refreshToken'));
+    const accessToken = request.cookies.get('accessToken');
+    const refreshToken = request.cookies.get('refreshToken');
+    if (!accessToken || !refreshToken) {
+      console.log('미들웨어 체크 : 로그인 필요함');
+      return NextResponse.redirect(new URL('/?type=login', request.url));
+    }
+  }
 }
 
 export const config = {
