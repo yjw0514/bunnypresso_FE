eacbe187ac8c8276eba13d99671d33216e42dbfa
yjw0514 <jwjwhappy92@gmail.com>
2023-08-03 12:19:14 +0900

    feat. 사용하지 않는 코드 삭제

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -4,12 +4,6 @@ import Profile from '@/components/MyPage/Profile';
 export default function MyPage() {
   return (
     <div className="px-4">
-      {/* <p className="fixed z-50 font-bold text-center -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-        준비중입니다.
-        <br /> 조금만 기다려주세요!
-      </p>
-      <div className="fixed top-0 bottom-0 left-0 right-0 z-40 bg-white/90 flex-center"></div> */}
-
       <h2 className="py-4 font-semibold">내 정보</h2>
       <Profile />
       <OrderList />
