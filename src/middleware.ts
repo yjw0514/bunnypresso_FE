583578095cd0fa964346db3adfab1357aebc42f8
yjw0514 <jwjwhappy92@gmail.com>
2023-08-03 15:42:19 +0900

    feat. 미들웨어 토큰이 둘 다 없는 경우 login 리다이렉트

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ export function middleware(request: NextRequest) {
   console.log('로그인 refreshToken!', request.cookies.get('refreshToken'));
   const accessToken = request.cookies.get('accessToken');
   const refreshToken = request.cookies.get('refreshToken');
-  if (!accessToken || !refreshToken) {
+  if (!accessToken && !refreshToken) {
     console.log('미들웨어 체크 : 로그인 필요함');
     return NextResponse.redirect(new URL('/login', request.url));
   }
