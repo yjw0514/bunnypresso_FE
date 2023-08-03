2e46c35bb79b9cd9f26bfb1d2b6d574e062843d4
yjw0514 <jwjwhappy92@gmail.com>
2023-08-03 14:26:15 +0900

    test. 콘솔추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  console.log(request);
   if (
     request.nextUrl.pathname.startsWith('/story') ||
     request.nextUrl.pathname.startsWith('/mypage')
